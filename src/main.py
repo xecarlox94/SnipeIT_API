cf4d817850e2d0c44d2d9a2bc26d7b243369cfbd
xecarlox94 <jf94.uk@gmail.com>
2023-10-17 16:13:38 +0100

    querying data from API

--- src/main.py
+++ src/main.py
@@ -1,15 +1,24 @@
 import os
+from math import ceil
 
 import requests
 from dotenv import load_dotenv
 
-
 load_dotenv()
-
-url = "https://develop.snipeitapp.com/api/v1/hardware?limit=2&offset=0&sort=created_at&order=desc"
-
 api_key = os.getenv("SNIPE_API_KEY")
 
+
+#total=35
+offset=0
+limit=0
+
+
+
+
+#exit(0)
+
+#url = "https://nationalrobotarium.snipe-it.io/api/v1/hardware?offset=0&limit=2&sort=created_at&order=desc"
+url = "https://nationalrobotarium.snipe-it.io/api/v1/hardware?offset="+ str(offset) +"&limit="+str(limit)+"&sort=created_at&order=desc"
 
 
 headers = {
@@ -18,6 +27,29 @@ headers = {
     "Authorization": "Bearer " + api_key
 }
 
+
 response = requests.get(url, headers=headers)
-print(response)
-print(response.text)
+
+body = response.json()
+
+robots = body['rows']
+
+
+
+
+get_robot_gen_info = lambda r: {
+    "id": r["id"],
+    "name": r["name"],
+    "location": r["location"],
+    "maintainer": r.get("custom_fields").get("Maintainer")
+}
+
+
+print(body['total'])
+print(body.keys())
+
+for r in robots:
+    # print(r.keys())
+    # print("\n\n")
+    # print(r['custom_fields'].keys())
+    print(get_robot_gen_info(r))
