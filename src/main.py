e0fc0c282bc918246f7927d9390baa4f7124fd83
xecarlox94 <jf94.uk@gmail.com>
2023-12-19 16:46:42 +0000

    working on different tables

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 import os, json
 from math import ceil
 
@@ -57,40 +58,11 @@ number_cell = lambda v: {
 
 
 
-get_row_robot = lambda r: {
-    "Name": title_cell(r['name']),
-    "ID": number_cell(r['id']),
-    "Category": text_cell(r['cat']['name']),
-    "Maintainer": text_cell(r['mnt']),
-    "Location": text_cell(r['loc']),
-}
-
-
-def get_gen_info_robot(robot):
-
-    mnt = robot.get("custom_fields").get("Maintainer")
-    if mnt is not None:
-        mnt = mnt.get("value")
-    else:
-        mnt = ""
-
-    loc = ""
-    if robot["location"] is not None:
-        loc = robot["location"]["name"]
-
-    return {
-        "id": robot["id"],
-        "name": robot["name"],
-        "loc": loc,
-        "cat": robot.get("category"),
-        "mnt": mnt
-    }
 
 
 def req_snipeit(f, end_point, body={}):
     url = f"https://nationalrobotarium.snipe-it.io/api/v1/{end_point}"
 
-    print(url)
     return f(
         url,
         headers={
@@ -102,7 +74,7 @@ def req_snipeit(f, end_point, body={}):
     ).json()
 
 
-get_body = lambda end_point: req_snipeit(
+req_get = lambda end_point: req_snipeit(
     requests.get,
     end_point
 )
@@ -121,192 +93,304 @@ req_patch = lambda end_point, body: req_snipeit(
 )
 
 
+
+def get_items_dict(
+        item_type,
+        get_gen_info_item=lambda x:x,
+        filterf=lambda i: True,
+        get_item_id=lambda i: i['id'],
+    ):
+
+    item_rows = req_get(f"{item_type}?offset=0&limit=0")['rows']
+
+    return dict(
+        map(
+            lambda item: (get_item_id(item), item),
+            filter(
+                filterf,
+                map(
+                    get_gen_info_item,
+                    item_rows
+                )
+            )
+        )
+    )
+
+
+
+def sync_table(
+        db_id,
+        items_dict,
+        get_db_entry_table,
+        get_row_item,
+        delete_row_item,
+    ):
+
+    database = notion.databases.query(
+        **{
+            "database_id": db_id
+        }
+    )
+
+    results = database['results']
+
+    db_entries_dict = dict(map(
+        get_db_entry_table,
+        results
+    ))
+
+
+    db_entries_keys = set(db_entries_dict.keys())
+    items_keys = set(items_dict.keys())
+
+    to_add_set = items_keys - db_entries_keys
+
+
+    to_update_set = db_entries_keys & items_keys
+
+
+    to_remove_set = db_entries_keys - items_keys
+
+
+
+    def create_row_page(item):
+        notion.pages.create(
+            parent={
+                "database_id": db_id
+            },
+            properties=get_row_item(item)
+        )
+
+
+    def update_row_page(item, page_id, archived=False):
+        notion.pages.update(
+            parent={
+                "database_id": db_id
+            },
+            properties=get_row_item(item),
+            page_id=page_id,
+            archived=archived
+        )
+
+    def delete_row_page(page_id, iid):
+
+        update_row_page(
+            delete_row_item(iid),
+            page_id,
+            archived=True
+        )
+
+
+
+    for iid in to_update_set:
+
+        item = items_dict[iid]
+        page_id = db_entries_dict[iid]
+
+        update_row_page(
+            item,
+            page_id
+        )
+
+
+    for iid in to_remove_set:
+
+        page_id = db_entries_dict[iid]
+
+        delete_row_page(page_id, iid)
+
+
+    for iid in to_add_set:
+
+        i = items_dict[iid]
+
+        create_row_page(i)
+
+
+
+
 # CONSUMABLES endpoint
 #
 #
 # post_consume = lambda
 # @app.route("...")
 # def consume_item():
+# CONSUMABLE_ID=9
+# USER_ID=5
+#
+# req_post(
+#     f"consumables/{CONSUMABLE_ID}/checkout",
+#     {
+#         "assigned_to": USER_ID
+#     }
+# )
+
+
 
 @app.route('/')
 def hello_geek():
 
-    """
-    ID=9
-
-    USER_ID=5
-
-    curl \
-        --request POST \
-        --url "$NR_URL/consumables/$ID/checkout" \
-        --header "Authorization: Bearer $API_KEY" \
-        --header "accept: application/json" \
-        --header "content-type: application/json" \
-        -d "{\"assigned_to\": $USER_ID}"
-    """
-    CONSUMABLE_ID=9
-
-    req_post(
-        f"consumables/{9}/checkout",
-        {
-            "assigned_to": 5
-        }
-    )
+    # ManufacturersSuppliers, 8b77ab855e8549da8bc9a85ac508e21d
+    # JustPeople, f97734ca13bc4fe9a76b2a2ac6fd95df
 
 
-    return "done"
 
-    robots_dict = dict(
-        map(
-            lambda r: (r['id'], r),
-            filter(
-                lambda r: True, #r['cat']['id'] != 6 and not r["id"] == 60,
-                map(
-                    get_gen_info_robot,
-                    body['rows']
-                )
-            )
-        )
-    )
-
-
-    db_id_robots = os.environ["NOTION_DB_ID"]
-    db_id_robots = "3412568aa7914734b22cd1a34b43ad7a"
-
-    get_db_entry_robots_table = lambda x: (
-        x['properties']['ID']['number'],
-        x['id']
-    )
-
-    delete_row_robot = lambda rid: {
-        "id": rid,
-        "name": "DELETE",
-        "loc": "DELETE",
-        "cat": {
-            'id': rid,
-            'name': "DELETE"
-        },
-        "mnt": "DELETE"
-    }
-
-
-    def sync_table(
-            db_id,
-            items_dict,
-            get_db_entry_table,
-            get_row_item,
-            delete_row_item,
+    def sync_assets(
+            db_id_assets,
+            filterf=lambda i: True,
         ):
 
-        database = notion.databases.query(
-            **{
-                "database_id": db_id
+
+        def get_gen_info_asset(asset):
+
+            # mnt = robot.get("custom_fields").get("Maintainer")
+            # if mnt is not None:
+                # mnt = mnt.get("value")
+            # else:
+                # mnt = ""
+            # loc = ""
+            # if robot["location"] is not None:
+                # loc = robot["location"]["name"]
+
+            return {
+                "id": asset["id"],
+                "name": asset["name"],
             }
+
+
+        get_db_entry_asset_table = lambda x: (
+            x['properties']['ID']['number'],
+            x['id']
         )
 
-        results = database['results']
 
-        db_entries_dict = dict(map(
-            get_db_entry_table,
-            results
-        ))
+        get_row_asset = lambda a: {
+            "Name": title_cell(a['name']),
+            "ID": number_cell(a['id']),
+        }
 
-        pprint(
-            list(
-                map(
-                    lambda x: x['properties']['ID'],
-                    filter(
-                        lambda x: True,
-                        results
-                    )
-                )
-            )
+
+        delete_row_asset = lambda a_id: {
+            "id": a_id,
+            "name": "DELETE",
+        }
+
+
+
+        assets_dict = get_items_dict(
+            "hardware",
+            get_gen_info_item=get_gen_info_asset,
+            filterf=filterf,
         )
 
-        # delete_row_page(page_id, iid)
 
-        #return
+        sync_table(
+            db_id_assets,
+            assets_dict,
+            get_db_entry_assets_table,
+            get_row_asset,
+            delete_row_asset
+        )
+    # Assets, 5e0b53e5311f4056b0b53ccd48755df6
+    # AssetsUnavailableRepair, fd3baece672c41bf820f7286111ff495
+    # AssetsWarrantyExpiring, 795d2cb803ff4fab8280a0c3798a6dac
 
 
-        db_entries_keys = set(db_entries_dict.keys())
-        items_keys = set(items_dict.keys())
-
-        to_add_set = items_keys - db_entries_keys
-
-
-        to_update_set = db_entries_keys & items_keys
-
-
-        to_remove_set = db_entries_keys - items_keys
-
+    return "Hello"
 
 
 
-        def create_row_page(item):
-            notion.pages.create(
-                parent={
-                    "database_id": db_id
-                },
-                properties=get_row_item(item)
-            )
+    #db_id_robots = os.environ["NOTION_DB_ID"]
+    db_id_robots = "3412568aa7914734b22cd1a34b43ad7a"
+
+    def sync_robots(
+            db_id, # Robots, 3412568aa7914734b22cd1a34b43ad7a
+            filterf=lambda i: True,
+        ):
 
 
-        def update_row_page(item, page_id, archived=False):
-            notion.pages.update(
-                parent={
-                    "database_id": db_id
-                },
-                properties=get_row_item(item),
-                page_id=page_id,
-                archived=archived
-            )
+        def get_gen_info_robot(robot):
 
-        def delete_row_page(page_id, iid):
+            mnt = robot.get("custom_fields").get("Maintainer")
+            if mnt is not None:
+                mnt = mnt.get("value")
+            else:
+                mnt = ""
 
-            update_row_page(
-                delete_row_item(iid),
-                page_id,
-                archived=True
-            )
+            loc = ""
+            if robot["location"] is not None:
+                loc = robot["location"]["name"]
 
-
-
-        for iid in to_update_set:
-
-            item = items_dict[iid]
-            page_id = db_entries_dict[iid]
-
-            update_row_page(
-                item,
-                page_id
-            )
-
-
-        for iid in to_remove_set:
-
-            page_id = db_entries_dict[iid]
-
-            delete_row_page(page_id, iid)
-
-
-        for iid in to_add_set:
-
-            i = items_dict[iid]
-
-            create_row_page(i)
+            return {
+                "id": robot["id"],
+                "name": robot["name"],
+                "loc": loc,
+                "cat": robot.get("category"),
+                "mnt": mnt
+            }
 
 
 
-    sync_table(
-        db_id,
-        robots_dict,
-        get_db_entry_robots_table,
-        get_row_robot,
-        delete_row_robot
+        get_db_entry_robots_table = lambda x: (
+            x['properties']['ID']['number'],
+            x['id']
+        )
+
+
+        get_row_robot = lambda r: {
+            "Name": title_cell(r['name']),
+            "ID": number_cell(r['id']),
+            "Category": text_cell(r['cat']['name']),
+            "Maintainer": text_cell(r['mnt']),
+            "Location": text_cell(r['loc']),
+        }
+
+
+        delete_row_robot = lambda rid: {
+            "id": rid,
+            "name": "DELETE",
+            "loc": "DELETE",
+            "cat": {
+                'id': rid,
+                'name': "DELETE"
+            },
+            "mnt": "DELETE"
+        }
+
+
+
+        robots_dict = get_items_dict(
+            "hardware",
+            get_gen_info_item=get_gen_info_robot,
+            filterf=lambda i: True,
+        )
+
+
+        pprint(robots_dict)
+
+
+        sync_table(
+            db_id_robots,
+            robots_dict,
+            get_db_entry_robots_table,
+            get_row_robot,
+            delete_row_robot
+        )
+
+    """
+    # ConsumablesPurchaseHistory, 817259b1f9ba45e583c335f7b19eb800
+    consumable_actions = get_items_dict(
+        "reports/activity",
+        filterf=lambda i: i['action_type'] == 'create new' and (
+            True #i['item']['type'] in ['consumable']
+        ),
     )
+    pprint(list(map(lambda i: i['item']['type'] == 'consumable', consumable_actions)))
+    """
 
 
     return '\n<h1>Updated Notion!</h1>\n'
+
 
 
 if __name__ == "__main__":
