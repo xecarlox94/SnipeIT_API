14320e3b58907ced34f78c9db3d954e20dd937fd
xecarlox94 <jf94.uk@gmail.com>
2024-01-17 16:32:46 +0000

    updating gen_env file

--- src/gen_env.sh
+++ src/gen_env.sh
@@ -5,11 +5,7 @@ read SNIPE_API_KEY
 echo "please insert NOTION_API_KEY"
 read NOTION_API_KEY
 
-echo "please insert NOTION_DB_ID"
-read NOTION_DB_ID
-
 echo -e "\
 SNIPE_API_KEY=$SNIPE_API_KEY\n\
-NOTION_API_KEY=$NOTION_API_KEY\n\
-NOTION_DB_ID=$NOTION_DB_ID\
+NOTION_API_KEY=$NOTION_API_KEY\
 " > .env
